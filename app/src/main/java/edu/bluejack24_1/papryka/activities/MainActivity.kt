7bc1ac30fa1b34b35098bb1f7566065eeae79b4d
Yenita Kimberly <yenitasiaoyen@gmail.com>
2024-08-29 15:33:55 +0700

    feat: bottom navbar, change fragments

--- app/src/main/java/edu/bluejack24_1/papryka/activities/MainActivity.kt
+++ app/src/main/java/edu/bluejack24_1/papryka/activities/MainActivity.kt
@@ -5,11 +5,55 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import edu.bluejack24_1.papryka.R
+import edu.bluejack24_1.papryka.databinding.ActivityMainBinding
+import edu.bluejack24_1.papryka.fragments.HomeFragment
+import edu.bluejack24_1.papryka.fragments.JobListFragment
+import edu.bluejack24_1.papryka.fragments.RoomFragment
+import edu.bluejack24_1.papryka.fragments.ScheduleFragment
+import edu.bluejack24_1.papryka.fragments.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.botNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menuHome -> {
+                    replaceFragment(HomeFragment())
+                }
+
+                R.id.menuJob -> {
+                    replaceFragment(JobListFragment())
+                }
+
+                R.id.menuSchedule -> {
+                    replaceFragment(ScheduleFragment())
+                }
+
+                R.id.menuRoom -> {
+                    replaceFragment(RoomFragment())
+                }
+
+                R.id.menuSettings -> {
+                    replaceFragment(SettingsFragment())
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
     }
 }
