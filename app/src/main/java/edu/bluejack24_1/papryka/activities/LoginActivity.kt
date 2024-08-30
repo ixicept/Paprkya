54dfd493edd44bd210d6a791175502500974eada
Philip <piusphilip.purnama@gmail.com>
2024-08-30 08:42:34 +0700

    feat! : Login with fingerprint (biometric)

--- app/src/main/java/edu/bluejack24_1/papryka/activities/LoginActivity.kt
+++ app/src/main/java/edu/bluejack24_1/papryka/activities/LoginActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import edu.bluejack24_1.papryka.databinding.ActivityLoginBinding
 import edu.bluejack24_1.papryka.models.LoginRequest
 import edu.bluejack24_1.papryka.utils.NetworkUtils
@@ -15,7 +18,8 @@ import kotlinx.coroutines.withContext
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -54,8 +58,50 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-        binding.btnBiometric.setOnClickListener {
-            // TODO: Biometric
+        val biometricManager = BiometricManager.from(this)
+        when (biometricManager.canAuthenticate()) {
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+
+                biometricPrompt = BiometricPrompt(this, ContextCompat.getMainExecutor(this), object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                        super.onAuthenticationError(errorCode, errString)
+                        Toast.makeText(this@LoginActivity, "Authentication error: $errString", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                        super.onAuthenticationSucceeded(result)
+                        val intentToHome = Intent(this@LoginActivity, MainActivity::class.java)
+                        startActivity(intentToHome)
+                        finish()
+                    }
+
+                    override fun onAuthenticationFailed() {
+                        super.onAuthenticationFailed()
+                        Toast.makeText(this@LoginActivity, "Authentication failed", Toast.LENGTH_SHORT).show()
+                    }
+                })
+
+                promptInfo = BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Biometric Login")
+                    .setSubtitle("Log in using your biometric credential")
+                    .setNegativeButtonText("Use password")
+                    .build()
+
+                binding.btnBiometric.setOnClickListener {
+                    biometricPrompt.authenticate(promptInfo)
+                }
+            }
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                Toast.makeText(this, "Biometric hardware not available", Toast.LENGTH_SHORT).show()
+            }
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                Toast.makeText(this, "Biometric hardware currently unavailable", Toast.LENGTH_SHORT).show()
+            }
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                Toast.makeText(this, "No biometric credentials enrolled", Toast.LENGTH_SHORT).show()
+                val intent = Intent(android.provider.Settings.ACTION_SECURITY_SETTINGS)
+                startActivity(intent)
+            }
         }
     }
 }
