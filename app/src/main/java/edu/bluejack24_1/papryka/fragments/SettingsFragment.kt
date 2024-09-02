cd36405f5debeb41dccf999e33b6008afc23d9d4
Yenita Kimberly <yenitasiaoyen@gmail.com>
2024-09-02 12:39:17 +0700

    store words to string and some ui fix

--- app/src/main/java/edu/bluejack24_1/papryka/fragments/SettingsFragment.kt
+++ app/src/main/java/edu/bluejack24_1/papryka/fragments/SettingsFragment.kt
@@ -5,16 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import edu.bluejack24_1.papryka.R
+import edu.bluejack24_1.papryka.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+
+    private lateinit var vBinding: FragmentSettingsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        vBinding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        val languageSpinner = vBinding.spLanguage
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.languages,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            languageSpinner.adapter = adapter
+        }
+
+        languageSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedNumber = parent.getItemAtPosition(position).toString()
+                Toast.makeText(requireContext(), "Selected: $selectedNumber", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Handle the case when nothing is selected, if necessary
+            }
+        }
+
+        return vBinding.root
     }
 
 }
